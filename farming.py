311a97d4ded5a5e78095d27b46e0ab9ebe78d427
welingtonVinicius <72838805+welingtonVinicius@users.noreply.github.com>
2024-06-11 04:25:43 +0200

    add new variables

--- farming.py
+++ farming.py
@@ -1,14 +1,14 @@
 import os
-import time
-import random
 import asyncio
 import logging
+from functools import lru_cache
 from dotenv import load_dotenv
 from sites_module import SiteA, SiteB
 
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+
 
 class PointFarmingAutomation:
     def __init__(self):
@@ -33,19 +33,28 @@ class PointFarmingAutomation:
         await asyncio.gather(*(site.stop_farming() for site in self.sites))
         self.is_farming = False
         self.progress = 0
+        self.clear_points_cache()
 
     async def monitor_farming(self):
         try:
             while self.is_farming:
-                await asyncio.sleep(10)  # Use asyncio.sleep() for asynchronous sleep
-                self.progress = sum(await site.get_points() for site in self.sites)
+                await asyncio.sleep(10)
+                self.progress = sum(await self.get_cached_points(site) for site in self.sites)
                 logging.info(f"Current farming progress: {self.progress} points")
         except KeyboardInterrupt:
             logging.info("Farming interrupted by user.")
             await self.stop_farming()
 
+    @lru_cache(maxsize=None)
+    async def get_cached_points(self, site):
+        return await site.get_points()
+
+    def clear_points_cache(self):
+        self.get_cached_points.cache_clear()
+
     def get_progress(self):
         return self.progress
+
 
 async def main():
     farming_automation = PointFarmingAutomation()
@@ -55,6 +64,7 @@ async def main():
         logging.error(f"An error occurred: {e}")
     finally:
         await farming_automation.stop_farming()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
